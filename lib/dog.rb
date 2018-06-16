f959d069428d171c955dcb055d82f00abf840026
Edward Mixon <edwardchase@gatech.edu>
2018-06-16 19:17:45 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog 
+  def name
+    @name = name
+  end
+  def name 
+    @name
+  end
+  
+  def breed 
+    @breed = breed
+  end
+   
+  def breed
+     @breed
+  end
+end
+
+class Person 
+  def name
+    @name = name
+  end
+  def name 
+    @name
+  end
+  
+  def job 
+    @job = job
+  end
+   
+  def job
+     @job
+  end
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
